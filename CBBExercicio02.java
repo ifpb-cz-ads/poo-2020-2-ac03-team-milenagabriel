08f75bd6b0db73398433b03bee933dda07af9ee9
repl.it user <replituser@example.com>
2021-02-16 20:22:50 +0000

    Exercicio 2 CBB

---
+++ CBBExercicio02.java
@@ -0,0 +1,10 @@
+class CBBExercicio02 {
+  public static void main (String[] args) {
+    int a = 12;
+    int b = 25;
+    int c = 15;
+    float soma = (float)a + (float)b  + (float)c ;
+    float media = soma / 3;
+    System.out.println("A média entre os 3 valores é " +media);
+  }
+}
---
+++ CBBExercicio03.java
@@ -0,0 +1,8 @@
+class CBBExercicio03 {
+  public static void main (String[] args) {
+    float base = 15;
+    float altura = 10;
+    float area= base * altura ;
+    System.out.println("A área do triângulo é " +area);
+  }
+}
